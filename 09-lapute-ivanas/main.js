5ab2e04d5c1fc00d84a4bd8abe1aa96d7c63e843
Vilija Kontrimaite <vikontrimaite@gmail.com>
2020-12-17 19:36:03 +0200

    initial structure of cats project

---
+++ 09-lapute-ivanas/main.js
@@ -0,0 +1,9 @@
+function upDate(element) {
+    document.getElementById('mainBox').innerHTML = element.alt;
+    document.getElementById('mainBox').style.background = "url('"+element.src+"')"
+}
+
+function unDo() {
+    document.querySelector('#mainBox').style.backgroundImage = "url('')";
+      document.querySelector('#mainBox').innerHTML = "Hover here again to choose the cat";
+}
--- 09-new/main.js
+++
@@ -1,2 +0,0 @@
-
-document.getElementById('quiz').innerHTML  ="New content!!";  
