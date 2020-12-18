99f42571e90a7e2f2c7798b3427e93eb4cd79a07
Vilija Kontrimaite <vikontrimaite@gmail.com>
2020-12-18 16:33:33 +0200

    update if statement

--- 10-hello-someone/main.js
+++ 10-hello-someone/main.js
@@ -3,9 +3,9 @@ let name = prompt(`What's your name?`);
 if (name.length !== 0) {
     document.write(`Hi, ${name}!`);
 }
-if(name == 'vi') {
-    document.write(`You have a beautiful name, ${name}!`);
-}
+// if(name == 'vi') {
+//     document.write(`You have a beautiful name, ${name}!`);
+// }
 else {
     document.write(`Feeling shy?..`)
 }
